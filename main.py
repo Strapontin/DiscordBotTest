708c4ed658f0a487106a13e6c77570af6cb29abb
Anthony <antodu73@hotmail.fr>
2021-06-03 08:45:12 +0000

    Added keep_alive.py to make the bot on permanently + several modifications

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # bot.py
 import os
-import datetime
 import discord
 from dotenv import load_dotenv
 from replit import db
+from keep_alive import keep_alive
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -19,13 +19,10 @@ async def manage_reaction(message):
   reac_a = ''
   reac_s = ''
 
-  print('__')
-
   # For each reaction in our message
   for reaction in message.reactions:
     async for user in reaction.users():
       if user != client.user:
-        print(datetime.datetime.now(), user, reaction)
 
         if reaction.emoji == '🇹':
           if reac_t != '':
@@ -83,17 +80,17 @@ async def on_message(message):
 
 @client.event
 async def on_reaction_add(reaction, user):
-  await manage_reaction(reaction.message)
+  if user != client.user:
+    await manage_reaction(reaction.message)
 
 
 @client.event
 async def on_raw_reaction_remove(payload):
-  print(db['channel_id'])
-
   channel = client.get_channel(db['channel_id'])
   message = await channel.fetch_message(db['msg_id'])
 
   await manage_reaction(message)
 
 
+keep_alive()
 client.run(TOKEN)
